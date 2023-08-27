a4000da195464931a58c2b71553f15179ef1807d
Chris Laprun <metacosm@gmail.com>
2023-08-27 09:36:44 +0200

    feat: initial status support using Watcher event source

--- src/main/java/io/halkyon/ExposedAppStatus.java
+++ src/main/java/io/halkyon/ExposedAppStatus.java
@@ -2,5 +2,30 @@ package io.halkyon;
 
 public class ExposedAppStatus {
 
-    // Add Status information here
+    private String host;
+    private String message;
+
+    public ExposedAppStatus() {
+    }
+
+    public ExposedAppStatus(String host, String message) {
+        this.host = host;
+        this.message = message;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
